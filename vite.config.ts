1488309b017a374287a8dfabb2c05c28591e3393
kexuan Wu <3033585934@qq.com>
2025-09-04 15:27:16 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./", // ✅ 关键：让静态资源路径相对化
+})
